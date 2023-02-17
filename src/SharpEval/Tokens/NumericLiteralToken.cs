0a85a1e45311b018c8a8d0fc954f4466e6741249
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 04:04:21 +0200

    Create NumericLiteralToken.cs

---
+++ src/SharpEval/Tokens/NumericLiteralToken.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace SharpEval.Tokens;
+
+public sealed class NumericLiteralToken : Token
+{
+	public double Value { get; }
+	
+	public NumericLiteralToken(double value)
+	{
+		Value = value;
+	}
+	
+	public override string ToString()
+	{
+		return Value.ToString(CultureInfo.InvariantCulture);
+	}
+	
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Value.ToString(CultureInfo.InvariantCulture)})";
+	}
+}
