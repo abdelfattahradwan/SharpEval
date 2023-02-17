eae6e88830d169a2299a09f4ae85f2c04ba2341d
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 05:17:37 +0200

    Create NumericLiteralExpression.cs

---
+++ src/SharpEval/Expressions/NumericLiteralExpression.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace SharpEval.Expressions;
+
+public sealed class NumericLiteralExpression : Expression
+{
+	public double Value { get; }
+	
+	public NumericLiteralExpression(double value)
+	{
+		Value = value;
+	}
+	
+	public override string ToString()
+	{
+		return Value.ToString(CultureInfo.InvariantCulture);
+	}
+	
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Value})";
+	}
+}
