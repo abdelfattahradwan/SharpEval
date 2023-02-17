6d530d5e26edc6e72eeec29e64d520efa26cd203
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 14:17:15 +0200

    Update UnitTest1.cs

--- src/SharpEval.Tests/UnitTest1.cs
+++ src/SharpEval.Tests/UnitTest1.cs
@@ -1,6 +1,5 @@
 using SharpEval.Expressions;
 using SharpEval.Tokens;
-using System.Diagnostics;
 using System.Globalization;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,26 +18,48 @@ public class UnitTest1
 	[Fact]
 	public void Test1()
 	{
-		const string input = "(2 + 2) * 1";
-		
-		Tokenizer tokenizer = new(input);
-		
-		Token[] tokens = tokenizer.ToArray();
-
-		foreach (Token token in tokens)
-		{
-			_testOutputHelper.WriteLine(token.ToDebugString());
-		}
-		
-		Parser parser = new(tokens);
-
-		Expression expression = parser.ParseExpression();
-		
-		_testOutputHelper.WriteLine($"{expression.ToDebugString()} ({expression})");
-		
+		// const string input = "(2 + 2) * 1";
+		//
+		// Tokenizer tokenizer = new(input);
+		//
+		// Token[] tokens = tokenizer.ToArray();
+		//
+		// foreach (Token token in tokens)
+		// {
+		// 	_testOutputHelper.WriteLine(token.ToDebugString());
+		// }
+		//
+		// Parser parser = new(tokens);
+		//
+		// Expression expression = parser.ParseExpression();
+		//
+		// _testOutputHelper.WriteLine($"{expression.ToDebugString()} ({expression})");
+		//
 		// double result = Interpreter.Evaluate(expression, new TestContext());
-		// 
+		//
 		// _testOutputHelper.WriteLine(result.ToString(CultureInfo.InvariantCulture));
+
+		Interpreter.DictionaryContext context = new()
+		{
+			Values =
+			{
+				["x"] = 10.0d,
+				["y"] = 20.0d,
+			},
+
+			Functions =
+			{
+				["sum"] = args => args.Sum(),
+			},
+		};
+
+		Token[] tokens = new Tokenizer(" sum ( x , y ) ").ToArray();
+
+		Expression expression = new Parser(tokens).ParseExpression();
+
+		double result = Interpreter.Evaluate(expression, context);
+
+		_testOutputHelper.WriteLine(result.ToString(CultureInfo.InvariantCulture)); // 30
 	}
 }
 
@@ -50,7 +71,7 @@ file class TestContext : Interpreter.IContext
 		{
 			"pi" => Math.PI,
 			"e" => Math.E,
-		
+
 			_ => throw new Exception($"Unknown identifier: {name}"),
 		};
 	}
@@ -64,7 +85,7 @@ file class TestContext : Interpreter.IContext
 			"tan" => args => Math.Tan(args[0]),
 			"sqrt" => args => Math.Sqrt(args[0]),
 			"pow" => args => Math.Pow(args[0], args[1]),
-		
+
 			_ => throw new Exception($"Unknown function: {name}"),
 		};
 	}
