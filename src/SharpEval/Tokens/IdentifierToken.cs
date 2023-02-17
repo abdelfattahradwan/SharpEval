e60bb720b7996ff85acff7773e276f22823b7db9
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 04:04:23 +0200

    Create IdentifierToken.cs

---
+++ src/SharpEval/Tokens/IdentifierToken.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpEval.Tokens;
+
+public sealed class IdentifierToken : Token
+{
+	public string Name { get; }
+	
+	public IdentifierToken(string name)
+	{
+		Name = name;
+	}
+	
+	public override string ToString()
+	{
+		return Name;
+	}
+	
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Name})";
+	}
+}
