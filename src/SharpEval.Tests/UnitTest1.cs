783dc44839b01893f42a108007647ca245ae89ee
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 05:17:30 +0200

    Update UnitTest1.cs

--- src/SharpEval.Tests/UnitTest1.cs
+++ src/SharpEval.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
+using SharpEval.Expressions;
 using SharpEval.Tokens;
 using System.Diagnostics;
+using System.Globalization;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -17,13 +19,51 @@ public class UnitTest1
 	[Fact]
 	public void Test1()
 	{
-		const string input = "11.6623156723451423641623461243651423651623416423651423661 + 2 * 3";
+		const string input = "(((53)-(2*(5-2)))+(3+(2/1)))";
 		
 		Tokenizer tokenizer = new(input);
+		
+		Token[] tokens = tokenizer.ToArray();
 
-		foreach (Token token in tokenizer)
+		foreach (Token token in tokens)
 		{
 			_testOutputHelper.WriteLine(token.ToDebugString());
 		}
+		
+		Parser parser = new(tokens);
+
+		Expression expression = parser.ParseExpression();
+		
+		double result = Interpreter.Evaluate(expression, new TestContext());
+		
+		_testOutputHelper.WriteLine(result.ToString(CultureInfo.InvariantCulture));
+	}
+}
+
+file class TestContext : Interpreter.IContext
+{
+	public double GetValue(string name)
+	{
+		return name switch
+		{
+			"pi" => Math.PI,
+			"e" => Math.E,
+		
+			_ => throw new Exception($"Unknown identifier: {name}"),
+		};
+	}
+
+	public Func<double[], double> GetFunction(string name)
+	{
+		return name switch
+		{
+			"sin" => args => Math.Sin(args[0]),
+			"cos" => args => Math.Cos(args[0]),
+			"tan" => args => Math.Tan(args[0]),
+			"sqrt" => args => Math.Sqrt(args[0]),
+			"pow" => args => Math.Pow(args[0], args[1]),
+		
+			_ => throw new Exception($"Unknown function: {name}"),
+		};
 	}
 }
