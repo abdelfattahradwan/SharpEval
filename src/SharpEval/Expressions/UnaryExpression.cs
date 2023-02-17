d93e0a2789c2deb53117097cca630daddc2309d5
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 05:17:39 +0200

    Create UnaryExpression.cs

---
+++ src/SharpEval/Expressions/UnaryExpression.cs
@@ -0,0 +1,25 @@
+﻿namespace SharpEval.Expressions;
+
+public sealed class UnaryExpression : Expression
+{
+	public Operator Operator { get; }
+	
+	public Expression Operand { get; }
+	
+	public UnaryExpression(Operator @operator, Expression operand)
+	{
+		Operator = @operator;
+		
+		Operand = operand;
+	}
+	
+	public override string ToString()
+	{
+		return $"{Operator.GetSymbol()}{Operand}";
+	}
+	
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Operator.GetSymbol()}, {Operand.ToDebugString()})";
+	}
+}
