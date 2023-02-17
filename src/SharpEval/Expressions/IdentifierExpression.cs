00bd93e2fa4931ea05350413c8fd94db87a22b63
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 05:17:36 +0200

    Create IdentifierExpression.cs

---
+++ src/SharpEval/Expressions/IdentifierExpression.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpEval.Expressions;
+
+public sealed class IdentifierExpression : Expression
+{
+	public string Name { get; }
+
+	public IdentifierExpression(string name)
+	{
+		Name = name;
+	}
+
+	public override string ToString()
+	{
+		return Name;
+	}
+
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Name})";
+	}
+}
