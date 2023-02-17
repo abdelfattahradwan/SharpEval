609621906118ef865c8d550102a91cc035549529
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 05:17:33 +0200

    Create BinaryExpression.cs

---
+++ src/SharpEval/Expressions/BinaryExpression.cs
@@ -0,0 +1,29 @@
+﻿namespace SharpEval.Expressions;
+
+public sealed class BinaryExpression : Expression
+{
+	public Expression Left { get; }
+	
+	public Operator Operator { get; }
+	
+	public Expression Right { get; }
+	
+	public BinaryExpression(Expression left, Operator @operator, Expression right)
+	{
+		Left = left;
+		
+		Operator = @operator;
+	
+		Right = right;
+	}
+	
+	public override string ToString()
+	{
+		return $"{Left} {Operator.GetSymbol()} {Right}";
+	}
+	
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Left.ToDebugString()}, {Operator.GetSymbol()}, {Right.ToDebugString()})";
+	}
+}
