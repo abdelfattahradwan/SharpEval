c0a8cbebe0d26dcfaa400a7b5733c0f96130b97c
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 04:28:00 +0200

    Update OperatorExtensions.cs

--- src/SharpEval/OperatorExtensions.cs
+++ src/SharpEval/OperatorExtensions.cs
@@ -4,50 +4,6 @@ namespace SharpEval;
 
 public static class OperatorExtensions
 {
-	public static bool IsUnary(this Operator @operator)
-	{
-		return @operator is Operator.UnaryPlus or Operator.UnaryMinus;
-	}
-
-	public static bool IsBinary(this Operator @operator)
-	{
-		return @operator is Operator.Plus or Operator.Minus or Operator.Times or Operator.Divide or Operator.Modulo;
-	}
-
-	public static bool IsAdditive(this Operator @operator)
-	{
-		return @operator is Operator.Plus or Operator.Minus;
-	}
-
-	public static bool IsMultiplicative(this Operator @operator)
-	{
-		return @operator is Operator.Times or Operator.Divide or Operator.Modulo;
-	}
-
-	public static bool IsLeftAssociative(this Operator @operator)
-	{
-		return @operator is Operator.Plus or Operator.Minus or Operator.Times or Operator.Divide or Operator.Modulo;
-	}
-
-	public static bool IsRightAssociative(this Operator @operator)
-	{
-		return @operator is Operator.UnaryPlus or Operator.UnaryMinus;
-	}
-
-	public static int GetPrecedence(this Operator @operator)
-	{
-		return @operator switch
-		{
-			Operator.Plus or Operator.Minus => 1,
-
-			Operator.Times or Operator.Divide or Operator.Modulo => 2,
-
-			Operator.UnaryPlus or Operator.UnaryMinus => 3,
-
-			_ => throw new Exception("Unreachable"),
-		};
-	}
-	
 	public static string GetSymbol(this Operator @operator)
 	{
 		return @operator switch
@@ -58,9 +14,6 @@ public static class OperatorExtensions
 			Operator.Times => "*",
 			Operator.Divide => "/",
 			Operator.Modulo => "%",
-			
-			Operator.UnaryPlus => "+",
-			Operator.UnaryMinus => "-",
 			
 			_ => throw new Exception("Unreachable"),
 		};
