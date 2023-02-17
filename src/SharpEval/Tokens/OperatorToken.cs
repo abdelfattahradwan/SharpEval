37720e3ccccb0a39a3eae4469bd523ae1653d19d
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-02-17 04:04:26 +0200

    Create OperatorToken.cs

---
+++ src/SharpEval/Tokens/OperatorToken.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpEval.Tokens;
+
+public sealed class OperatorToken : Token
+{
+	public Operator Operator { get; }
+
+	public OperatorToken(Operator @operator)
+	{
+		Operator = @operator;
+	}
+
+	public override string ToString()
+	{
+		return Operator.GetSymbol();
+	}
+
+	public override string ToDebugString()
+	{
+		return $"{base.ToDebugString()}({Operator})";
+	}
+}
